61f0434120562f65e74e3f010dc7ab2f8ee1afef
Seo Master <71407031+Savvymag@users.noreply.github.com>
2020-09-29 01:50:00 +0100

    Create ic.js

---
+++ ic.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+$(&#39;.post-body a[href]:has(img)&#39;).css({&quot;cursor&quot;: &quot;default&quot; , &quot;pointer-events&quot;: &quot;none&quot; , &quot;-webkit-user-select&quot;: &quot;none&quot; , &quot;-khtml-user-select&quot;: &quot;none&quot; , &quot;-moz-user-select&quot;: &quot;none&quot; , &quot;-o-user-select&quot;: &quot;none&quot; , &quot;user-select&quot;: &quot;none&quot;})
+.click(function() { return false; });
+});
